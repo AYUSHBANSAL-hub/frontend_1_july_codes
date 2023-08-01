164f1fdf4c9215907fb89ec54b9112a0a9d470e7
ayush bansal <forstudyonly101@gmail.com>
2023-08-01 20:04:01 +0530

    djdnj

---
+++ 31st July/js-1/script.js
@@ -0,0 +1,56 @@
+// What is Js?
+// Js allows developers to manipulate and modify content (clear)
+// respond to user actions (clear)
+// create interactive elements
+
+
+// const btn= document.getElementById('btn');
+// btn.addEventListener('click',()=>{
+//    document.getElementById('content').innerText="Hey peeps";
+// let mydiv=`<div class="box"></div>`
+// for(let i=0;i<100;i++){
+//     document.getElementById("my-div").innerHTML+=mydiv;
+// }
+// })
+// 1. Outputting text in console
+console.log("Hello World");
+
+// 2. Variables and Data Types
+let message="My name is Ayush";
+let num=19;
+let isTrue=true;
+console.log(typeof message, message);
+console.log(typeof num);
+console.log(isTrue);
+//
+// 3. Arithmetic operations
+// let a=+"3";
+let sum=10+10;
+let difference=10-1;
+let modulo=10%3;
+
+let fname="Ayush";
+console.log("Hello user, I have received your name as ",fname)
+console.log(sum,difference,modulo);
+console.log(sum);
+console.log(difference);
+console.log(modulo);
+console.log(3+3);
+console.log(3*3);
+console.log(3/3);
+console.log(3-3);
+
+// 4. conditionals
+let age=79;
+if(age<19){
+    console.log("you are a kid");
+}else if(age<60){
+    console.log("You are an adult");
+}else console.log("You are a senior citizen");
+
+//5. Input from user
+
+// let username=prompt("Please enter your name");
+console.log("Hello",username, "nice to meet you!");
+
+
